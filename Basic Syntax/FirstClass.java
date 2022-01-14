c30b7706853bc0d0be2988109277a83a0533a203
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-01-14 23:53:06 +0500

    Create FirstClass.java

---
+++ Basic Syntax/FirstClass.java
@@ -0,0 +1,8 @@
+class FirstClass{
+    public static void main(String args[]){
+        System.out.print("Welcome in Java Programming \n");
+        System.out.println("Hello Wrold");
+        System.out.println("*\n**\n***\n****");
+        
+    }
+}
