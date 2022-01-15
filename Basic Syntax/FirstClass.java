e80d4cf2025e848c012f32a30983f6da1677f353
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-01-15 17:54:23 +0500

    Learn variabel , datatype , input / output  in java syntax

--- Basic Syntax/FirstClass.java
+++ Basic Syntax/FirstClass.java
@@ -1,8 +1,30 @@
+import java.util.*;
+
 class FirstClass{
     public static void main(String args[]){
         System.out.print("Welcome in Java Programming \n");
         System.out.println("Hello Wrold");
         System.out.println("*\n**\n***\n****");
+
+        // Variables + Datatype
+        int a = 20;
+        int b =30;
+        int s = a + b ;
+        System.out.println(s);
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        System.out.println(name);
+        // nextInt()
+        // nextFloat()
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println(sum);
+
         
     }
 }
