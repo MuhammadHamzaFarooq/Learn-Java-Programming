40b59ba24ad42acf006c223b14ebf5c2ce3fca14
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-01-18 19:24:01 +0500

    learn and solve half pyramid and  inverse half pyramid Question

--- 9 Best Pattern Questions/Pattern.java
+++ 9 Best Pattern Questions/Pattern.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.synth.SynthColorChooserUI;
+
 class Pattern {
     public static void main(String[] args) {
 
@@ -8,12 +10,12 @@ class Pattern {
           *****
           *****
         */
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print("*");
-            }
-            System.out.println("*");
-        }
+        // for (int i = 0; i < 4; i++) {
+        //     for (int j = 0; j < 4; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println("*");
+        // }
 
         // Q2 : Hollow Rectangle 
         /*
@@ -29,16 +31,50 @@ class Pattern {
         int m = 5;
 
         // outer Loop 
-        for(int i =1 ; i<=n ; i++){
-            for(int j = 1 ; j<=m; j++){
-                if(i == 1 || j == 1 || i == n || j == m){
-                 System.out.print('*');
-                }
-                else{
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
+    //    for(int i = 1 ; i<=n ; i++){
+    //       //inner loop 
+    //       for(int j = 1; j<=m ; j++){
+    //           if(i==1 || j == 1 || i == n || j == m){
+    //             System.out.print("*");
+    //           }
+    //           else{
+    //               System.out.print(" ");
+    //           }
+    //         }
+    //         System.out.println();
+    //    }
+
+       //Q3 : Half Payramid 
+
+       /*
+         *
+         * *
+         * * *
+         * * * *
+       */
+    //   int row = 4 ;
+    //   for(int i = 1 ; i <=row ; i++){
+    //       //inner loop 
+    //       for(int j = 1 ; j <=i ; j++){
+    //         System.out.print("*");
+    //       }
+    //          System.out.println();
+    //   }
+    
+    // Q4 :
+    /*
+       * * * *
+       * * *
+       * * 
+       * 
+    */
+      for(int i = 4 ; i >=1 ; i--){
+          //inner loop 
+          for(int j = 1 ; j <=i ; j++){
+            System.out.print("*");
+          }
+          System.out.println();
+      }
+
     }
 }
